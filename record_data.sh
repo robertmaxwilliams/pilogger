67442d8bce4b14b26e6400a0dbfd48848579f743
Robert <robertmaxwilliams@gmail.com>
2017-07-11 19:42:05 -0400

    Update record_data.sh
    I hope it works

--- record_data.sh
+++ record_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-while :
-do
-	echo "recording video and accel data"
-	
 
-done
+python record_gyro.py &
+echo "recording video and accel data"
+raspivid --width 640 --height 360 --framerate 24 --bitrate 750000 --qp 20 --timeout $((30*60*1000)) --segment $((60*1000)) --output video%(%s)T\n.h264
+echo "thirty mins is up, stop record"
+
