12f0d25a53a7603e84adb906b3f8808df7fdd412
robertmaxwilliams <robertmaxwilliams@gmail.com>
2017-07-12 19:44:35 +0000

    changed to %d naming and 1 minute segments

--- record.sh
+++ record.sh
@@ -3,5 +3,5 @@
 python record_gyro.py &
 echo "recording video and accel data"
 epochTime=$(date +%s)
-raspivid -n -w 640 -h 480 --timeout 1800000 --segment 5000 --output "video $epochTime.h264" &
+raspivid -n -w 640 -h 480 --timeout 1800000 --segment 60000 --output "video%d.h264" &
 PICAM=$!
